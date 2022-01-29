025b767f86998a8b5c455bd89f4db877462c4610
nnichols <nichols1991@gmail.com>
2022-01-29 10:13:23 -0600

    Use antq to perform updates, not bash

--- dependency-check.sh
+++ dependency-check.sh
@@ -10,6 +10,10 @@ for artifact in $EXCLUDE; do
     EXCLUDES="${EXCLUDES} --exclude=${artifact}"
 done
 
+if [ -z "${DIRECTORY}" ]; then
+    DIRECTORY="."
+fi
+
 DIRECTORIES=""
 for directory in $DIRECTORY; do
     DIRECTORIES="${DIRECTORIES} --directory=${directory}"
@@ -26,21 +30,12 @@ UPGRADES=$(clojure -Sdeps '{:deps {antq/antq {:mvn/version "RELEASE"}}}' -m antq
 if [ $BATCH = 'true' ]; then
   BRANCH_NAME="dependencies/clojure/$(date +"%Y-%m-%d-%H-%M-%S")"
   git checkout -b $BRANCH_NAME
-  for upgrade in $UPGRADES; do
-    IFS=',' temp=($upgrade)
-    DEP_NAME=${temp[0]}
-    OLD_VERSION=${temp[1]}
-    NEW_VERSION=${temp[2]}
-    echo "Updating" $DEP_NAME "version" $OLD_VERSION "to" $NEW_VERSION
-    ESCAPED_DEP_NAME=`echo $DEP_NAME | sed 's/\//\\\\\//'`
-    sed -e "/$ESCAPED_DEP_NAME/s/$OLD_VERSION/$NEW_VERSION/" deps.edn > deps2.edn
-    mv deps2.edn deps.edn
-    git add .
-    git commit -m "Bump $DEP_NAME from $OLD_VERSION to $NEW_VERSION"
-  done
+  echo "Updating all dependencies"
+  clojure -Sdeps '{:deps {antq/antq {:mvn/version "RELEASE"}}}' -m antq.core --upgrade --force $EXCLUDES $DIRECTORIES $SKIPS
+  git add .
+  git commit -m "Update all outdated dependencies"
   git push -u "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git" $BRANCH_NAME
   gh pr create --fill --head $BRANCH_NAME --base $BRANCH
-  echo
   git checkout $BRANCH
 else
   for upgrade in $UPGRADES; do
@@ -52,9 +47,7 @@ else
     echo "Updating" $DEP_NAME "version" $OLD_VERSION "to" $NEW_VERSION
     git checkout -b $BRANCH_NAME
     if [[ $? == 0 ]]; then
-      ESCAPED_DEP_NAME=`echo $DEP_NAME | sed 's/\//\\\\\//'`
-      sed -e "/$ESCAPED_DEP_NAME/s/$OLD_VERSION/$NEW_VERSION/" deps.edn > deps2.edn
-      mv deps2.edn deps.edn
+      clojure -Sdeps '{:deps {antq/antq {:mvn/version "RELEASE"}}}' -m antq.core --upgrade --force --directory=${dir} --focus=${DEP_NAME} || echo "Cannot update $DEP_NAME. Continuing" && git checkout $BRANCH && continue
       git add .
       git commit -m "Bump $DEP_NAME from $OLD_VERSION to $NEW_VERSION"
       git push -u "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git" $BRANCH_NAME
